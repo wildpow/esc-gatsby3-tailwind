c9819b6c5ca643211164af7735f5d58849f3706d
wildpow <element1028@gmail.com>
2021-04-10 09:43:52 -0700

    refactor: tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,57 @@
+const colors = require("tailwindcss/colors");
+const typography = require("@tailwindcss/typography");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        "Roboto",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      serif: [
+        "Roboto Slab",
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+      ],
+    },
+
+    extend: {
+      screens: {
+        xms: "400px",
+        phablet: "550px",
+      },
+      colors: {
+        brandBackground: colors.gray[100],
+        brandBlue: colors.lightBlue[900],
+        brandRed: colors.red[900],
+        coolGray: colors.coolGray,
+        blueGray: colors.blueGray,
+        warmGray: colors.warmGray,
+        lightBlue: colors.lightBlue,
+        rose: colors.rose,
+        cyan: colors.cyan,
+        indigo: colors.indigo,
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  variants: {},
+  plugins: [typography],
+};
