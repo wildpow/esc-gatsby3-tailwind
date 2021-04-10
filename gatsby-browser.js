4c317b233815c8ed2a74315b2d0ebab0df62b271
wildpow <element1028@gmail.com>
2021-04-10 09:44:21 -0700

    refactor: added back fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
-import './src/styles/global.css';
+import "./src/styles/global.css";
+import "@fontsource/roboto";
+import "@fontsource/roboto/300.css"; // footer
+import "@fontsource/roboto/500.css"; // footer
+import "@fontsource/roboto/900.css"; // logo
+import "@fontsource/roboto/900-italic.css"; // logo
+import "@fontsource/roboto-slab";
