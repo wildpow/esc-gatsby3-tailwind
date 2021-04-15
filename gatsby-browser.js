b0e373a9aaa03a1a5de954eb3ae50858e5af47ca
wildpow <element1028@gmail.com>
2021-04-14 20:38:40 -0700

    refactor: new font weight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,8 @@ import "./src/styles/global.css";
 import "@fontsource/roboto";
 import "@fontsource/roboto/300.css"; // footer
 import "@fontsource/roboto/500.css"; // footer
+import "@fontsource/roboto/700.css"; // Contact-us h2 -
 import "@fontsource/roboto/900.css"; // logo
 import "@fontsource/roboto/900-italic.css"; // logo
 import "@fontsource/roboto-slab";
+import "@fontsource/roboto-slab/300.css"; // warrant-p
