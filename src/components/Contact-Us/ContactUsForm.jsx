1c1c003b0f240db7b881ff1a88a562865d4be584
wildpow <element1028@gmail.com>
2021-04-15 18:39:03 -0700

    style: contact us page

--- src/components/Contact-Us/ContactUsForm.jsx
+++ src/components/Contact-Us/ContactUsForm.jsx
@@ -1,131 +1,10 @@
 /* eslint-disable react/jsx-curly-newline */
-import React from "react"
+import React, { useReducer } from "react";
+import { navigate } from "gatsby";
 
-// import { useReducer } from "react";
-// import styled from "styled-components";
-// import { navigate } from "gatsby";
-// import {
-//   colors,
-//   spacing,
-//   fontSize,
-//   breakpoints,
-//   radius,
-// } from "../../utils/styles";
 // import { PrimaryButton } from "../shared/Buttons";
-// import SendIcon from "../../assets/paper-plane-solid.svg";
-// import ResetIcon from "../../assets/redo-solid.svg";
-
-// const FormRoot = styled.form`
-//   border-radius: ${radius.large}px;
-//   padding: ${spacing["1"]};
-//   fieldset {
-//     border: none;
-//   }
-//   .container {
-//     display: flex;
-//     justify-content: space-between;
-//     flex-direction: column;
-//   }
-
-//   label {
-//     display: flex;
-//     flex-direction: column;
-//   }
-//   textarea {
-//     min-height: 150px;
-//     font-size: ${fontSize.base};
-//     padding: ${spacing[2]};
-//     border: 1px solid ${colors.gray["500"]};
-//     resize: none;
-//     outline: none;
-//     :focus {
-//       box-shadow: 0px 5px 5px 0px ${colors.gray["500"]};
-//       outline: 0;
-//       transition: box-shadow 0.15s ease-in-out;
-//     }
-//   }
-//   .input-label {
-//     input:-internal-autofill-selected {
-//       background-color: transparent;
-//     }
-//     input:-webkit-autofill,
-//     input:-webkit-autofill:hover,
-//     input:-webkit-autofill:focus,
-//     textarea:-webkit-autofill,
-//     textarea:-webkit-autofill:hover,
-//     textarea:-webkit-autofill:focus,
-//     select:-webkit-autofill,
-//     select:-webkit-autofill:hover,
-//     select:-webkit-autofill:focus {
-//       border: 1px solid ${colors.blue[900]};
-//       -webkit-text-fill-color: ${colors.blue[800]};
-//       box-shadow: 0 0 0px 1000px transparent inset;
-//       transition: background-color 5000s ease-in-out 0s;
-//     }
-
-//     width: 100%;
-//     font-size: ${fontSize.sm};
-//     span {
-//       padding-bottom: ${spacing[1]};
-//       color: ${colors.gray[900]};
-//     }
-//     input {
-//       width: 100%;
-//       padding: 10px 5px;
-//       font-size: 16px;
-//       color: ${colors.gray["900"]};
-//       margin-bottom: 30px;
-//       border: 1px solid ${colors.gray["500"]};
-//       outline: none;
-//       :focus {
-//         box-shadow: 0px 5px 5px 0px ${colors.gray["500"]};
-//         outline: 0;
-//         transition: box-shadow 0.15s ease-in-out;
-//       }
-//     }
-//   }
-
-//   .bottom-buttons {
-//     display: flex;
-//     justify-content: space-between;
-//     padding-top: ${spacing["8"]};
-//   }
-
-//   @media (min-width: ${breakpoints.sm}) {
-//     border: 1px solid ${colors.gray["300"]};
-//     border-radius: ${radius.large}px;
-//     padding: ${spacing["6"]};
-//     .container {
-//       flex-direction: row;
-//       justify-content: space-between;
-//     }
-//     .input-label {
-//       width: 45%;
-//     }
-//     .message {
-//       font-size: ${fontSize["2xl"]};
-//     }
-//     textarea {
-//       padding: ${spacing[6]};
-//     }
-//   }
-//   @media (min-width: ${breakpoints.lg}) {
-//     .input-label {
-//       font-size: ${fontSize.base};
-//       font-weight: 700;
-//       span {
-//         padding-left: 2px;
-//       }
-//       input {
-//         padding: 15px 10px;
-//         font-size: ${fontSize.lg};
-//       }
-//     }
-//     textarea {
-//       font-size: ${fontSize.lg};
-//     }
-//   }
-// `;
+import SendIcon from "../../svg/paper-plane-solid.svg";
+import ResetIcon from "../../svg/redo-solid.svg";
 
 const initialState = {
   firstName: "",
@@ -134,168 +13,184 @@ const initialState = {
   tel: "",
   type: "",
   message: "",
-}
+};
 const reducer = (state, action) => {
   switch (action.type) {
     case "firstName":
-      return { ...state, firstName: action.payload }
+      return { ...state, firstName: action.payload };
     case "lastName":
-      return { ...state, lastName: action.payload }
+      return { ...state, lastName: action.payload };
     case "email":
-      return { ...state, email: action.payload }
+      return { ...state, email: action.payload };
     case "tel":
-      return { ...state, tel: action.payload }
+      return { ...state, tel: action.payload };
     case "type":
-      return { ...state, type: action.payload }
+      return { ...state, type: action.payload };
     case "message":
-      return { ...state, message: action.payload }
+      return { ...state, message: action.payload };
     case "reset":
-      return { ...initialState }
+      return { ...initialState };
     default:
-      throw new Error()
+      throw new Error();
   }
-}
+};
 // const FormButtons = styled(PrimaryButton)`
 //   padding-right: 20px;
 //   padding-left: 20px;
 // `;
 
-const encode = data => {
-  return Object.keys(data)
+const encode = data =>
+  Object.keys(data)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join("&")
-}
+    .join("&");
 const ContactUsForm = () => {
-  // const [state, dispatch] = useReducer(reducer, initialState);
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const form = e.target;
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": form.getAttribute("name"),
-  //       ...state,
-  //     }),
-  //   })
-  //     .then(() => navigate(form.getAttribute("action")))
-  //     .catch((error) => alert(error));
-  // };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
   return (
-    // <FormRoot
-    //   name="contact"
-    //   method="post"
-    //   action="/thank-you"
-    //   data-netlify="true"
-    //   data-netlify-honeypot="bot-field"
-    //   onSubmit={handleSubmit}
-    // >
-    //   <input type="hidden" name="form-name" value="contact" />
-    //   <p hidden>
-    //     <label htmlFor="bot-field">
-    //       Don’t fill this out:
-    //       <input
-    //         name="bot-field"
-    //         onChange={() => console.log("Don’t fill this out")}
-    //       />
-    //     </label>
-    //   </p>
+    <form
+      name="contact"
+      method="post"
+      action="/thank-you"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}
+      className="sm:rounded sm:border-blueGray-400 sm:border p-1 sm:p-6"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label htmlFor="bot-field">
+          Don’t fill this out:
+          <input
+            name="bot-field"
+            onChange={() => console.log("Don’t fill this out")}
+          />
+        </label>
+      </p>
 
-    //   <fieldset>
-    //     <div className="container">
-    //       <label htmlFor="firstName" id="firstName" className="input-label">
-    //         <span>Fist Name</span>
-    //         <input
-    //           placeholder="Percival"
-    //           required
-    //           type="text"
-    //           id="firstName"
-    //           name="firstName"
-    //           autoComplete="given-name"
-    //           value={state.firstName}
-    //           onChange={(e) =>
-    //             dispatch({ type: "firstName", payload: e.target.value })
-    //           }
-    //         />
-    //       </label>
+      <fieldset>
+        <div className="flex justify-between flex-col sm:flex-row sm:mb-6">
+          <label
+            htmlFor="firstName"
+            id="firstName"
+            className="input-label flex flex-col w-full sm:w-5/12 mb-6 sm:mb-0"
+          >
+            <span className="font-medium">First Name</span>
+            <input
+              className="rounded-sm border-blueGray-300 shadow "
+              placeholder="Percival"
+              required
+              type="text"
+              id="firstName"
+              name="firstName"
+              autoComplete="given-name"
+              value={state.firstName}
+              onChange={e =>
+                dispatch({ type: "firstName", payload: e.target.value })
+              }
+            />
+          </label>
 
-    //       <label htmlFor="lastName" id="lastName" className="input-label">
-    //         <span>Last Name</span>
-    //         <input
-    //           required
-    //           type="text"
-    //           id="lastName"
-    //           name="lastName"
-    //           autoComplete="family-name"
-    //           placeholder="Panda"
-    //           value={state.lastName}
-    //           onChange={(e) =>
-    //             dispatch({ type: "lastName", payload: e.target.value })
-    //           }
-    //         />
-    //       </label>
-    //     </div>
-    //     <div className="container">
-    //       <label htmlFor="tel" className="input-label">
-    //         <span>Phone</span>
-    //         <input
-    //           autoComplete="tel-national"
-    //           pattern="^[0-9-+s()]*$"
-    //           type="tel"
-    //           name="tel"
-    //           placeholder="***-***-****"
-    //           value={state.tel}
-    //           onChange={(e) =>
-    //             dispatch({ type: "tel", payload: e.target.value })
-    //           }
-    //         />
-    //       </label>
-    //       <label htmlFor="email" className="input-label">
-    //         <span>Email</span>
-    //         <input
-    //           required
-    //           placeholder="youremail@email.com"
-    //           type="email"
-    //           name="email"
-    //           autoComplete="email"
-    //           value={state.email}
-    //           onChange={(e) =>
-    //             dispatch({ type: "email", payload: e.target.value })
-    //           }
-    //         />
-    //       </label>
-    //     </div>
+          <label
+            htmlFor="lastName"
+            id="lastName"
+            className="input-label flex flex-col w-full sm:w-5/12 mb-6 sm:mb-0"
+          >
+            <span>Last Name</span>
+            <input
+              className="rounded-sm border-blueGray-300 shadow"
+              required
+              type="text"
+              id="lastName"
+              name="lastName"
+              autoComplete="family-name"
+              placeholder="Panda"
+              value={state.lastName}
+              onChange={e =>
+                dispatch({ type: "lastName", payload: e.target.value })
+              }
+            />
+          </label>
+        </div>
+        <div className="flex justify-between flex-col sm:flex-row mb-6">
+          <label
+            htmlFor="tel"
+            className="input-label flex flex-col w-full sm:w-5/12 mb-6 sm:mb-0"
+          >
+            <span>Phone</span>
+            <input
+              className="rounded-sm border-blueGray-300 shadow"
+              autoComplete="tel-national"
+              pattern="^[0-9-+s()]*$"
+              type="tel"
+              name="tel"
+              placeholder="***-***-****"
+              value={state.tel}
+              onChange={e => dispatch({ type: "tel", payload: e.target.value })}
+            />
+          </label>
+          <label
+            htmlFor="email"
+            className="input-label flex flex-col w-full sm:w-5/12 mb-6 sm:mb-0"
+          >
+            <span>Email</span>
+            <input
+              className="rounded-sm border-blueGray-300 shadow"
+              required
+              placeholder="youremail@email.com"
+              type="email"
+              name="email"
+              autoComplete="email"
+              value={state.email}
+              onChange={e =>
+                dispatch({ type: "email", payload: e.target.value })
+              }
+            />
+          </label>
+        </div>
 
-    //     <label htmlFor="message" className="message">
-    //       <textarea
-    //         onChange={(e) =>
-    //           dispatch({ type: "message", payload: e.target.value })
-    //         }
-    //         placeholder="Enter your message here..."
-    //         required
-    //         type="text"
-    //         value={state.message}
-    //         name="message"
-    //       />
-    //     </label>
-    //     <div className="bottom-buttons">
-    //       <FormButtons
-    //         danger
-    //         type="button"
-    //         onClick={() => dispatch({ type: "reset" })}
-    //       >
-    //         Reset
-    //         <ResetIcon />
-    //       </FormButtons>
-    //       <FormButtons type="submit">
-    //         Submit
-    //         <SendIcon />
-    //       </FormButtons>
-    //     </div>
-    //   </fieldset>
-    // </FormRoot>
-    <div>feewfef</div>
-  )
-}
+        <label htmlFor="message" className="message flex flex-col">
+          <textarea
+            onChange={e =>
+              dispatch({ type: "message", payload: e.target.value })
+            }
+            className="rounded-sm border-blueGray-300 shadow"
+            placeholder="Enter your message here..."
+            required
+            type="text"
+            value={state.message}
+            name="message"
+          />
+        </label>
+        <div className="bottom-buttons">
+          <button
+            danger
+            type="button"
+            onClick={() => dispatch({ type: "reset" })}
+          >
+            Reset
+            <ResetIcon />
+          </button>
+          <button type="submit">
+            Submit
+            <SendIcon />
+          </button>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
 
-export default ContactUsForm
+export default ContactUsForm;
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -3,7 +3,9 @@ import { graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import PropTypes from "prop-types";
+import ContactInfo from "../components/Contact-Us/ContactInfo";
 import Layout from "../components/Layout";
+import ContactUsForm from "../components/Contact-Us/ContactUsForm";
 
 const ContactUs = ({ data }) => {
   const { panda, seo } = data;
@@ -12,17 +14,22 @@ const ContactUs = ({ data }) => {
       <HelmetDatoCms seo={seo.seoMetaTags} />
       <section>
         <GatsbyImage image={panda.childImageSharp.gatsbyImageData} />
-        <div className="xl:mx-auto sm:mx-5 relative z-10 max-w-6xl p-3 mx-auto -mt-5 bg-white rounded-lg rounded-b-none shadow-lg xl:-mt-24 lg:-mt-20 md:p-10 phablet:-mt-12 text-blueGray-800 sm:p-10">
-          <h2 className="font-sans text-2xl font-bold border-b-4 border-red-800 sm:text-3xl text-blueGray-700 lg:text-5xl">
+        <div className="xl:mx-auto sm:mx-5 relative z-10 max-w-6xl p-3 mx-auto -mt-5 bg-white rounded-lg rounded-b-none shadow-lg xl:-mt-24 lg:-mt-20 md:p-10 phablet:-mt-12  sm:p-10">
+          <h2 className="text-2xl heading-underline sm:text-3xl lg:text-5xl">
             Contact Us
           </h2>
-          <p className="pb-2 font-serif text-base sm:text-xl sm:pb-4 lg:text-2xl lg:pb-6">
+          <p className="pb-2 text-base sm:text-xl sm:pb-4 lg:text-2xl lg:pb-6 my-6 ">
             Thank you for visiting ESC Mattress Center, Snohomish County&apos;s
             favorite mattress store. We strive to be the best mattress store we
             can be and are always here to help. Feel free to contact us in
             person at the store during normal business hours, by phone, or
             through the form below. Sleep well!
           </p>
+          <ContactInfo />
+          <h3 className="heading-underline text-2xl xl:text-3xl mb-6">
+            How can We help?
+          </h3>
+          <ContactUsForm />
         </div>
       </section>
       contact us
