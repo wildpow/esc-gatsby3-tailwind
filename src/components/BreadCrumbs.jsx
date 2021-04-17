d4ffa4d7ff793a662cdcd25758cda7335acf6bbd
wildpow <element1028@gmail.com>
2021-04-16 18:35:26 -0700

    refactor: breadcrumb logic

--- src/components/BreadCrumbs.jsx
+++ src/components/BreadCrumbs.jsx
@@ -1,15 +1,15 @@
-// import PropTypes from "prop-types";
-// import { Link } from "gatsby";
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
 // import styled from "styled-components";
 // import { DisplayNonePr1nt } from "../styles/_pr1nt/main";
 
-// function ifLavish(string) {
-//   if (string === "posh-and-lavish") {
-//     return string.split("-").join(" ");
-//   }
-//   return string;
-// }
-import React from "react"
+function ifLavish(string) {
+  if (string === "posh-and-lavish") {
+    return string.split("-").join(" ");
+  }
+  return string;
+}
 
 // export const BreadWrapper = styled.div`
 //   padding-top: 15px;
@@ -20,13 +20,11 @@ import React from "react"
 //   color: ${(props) => props.theme.newColor2};
 //   font-family: ${(props) => props.theme.MainFont1};
 //   display: flex;
-//   /* margin-right: ${(props) => (props.Brands ? "15px" : "5px")};
-//   margin-left: ${(props) => (props.Brands ? "15px" : "5px")}; */
+
 //   @media (min-width: 568px) {
 //     font-size: 1rem;
 //     padding-left: 7px;
-//     /* margin-right: ${(props) => (props.Brands ? "45px" : "5px")};
-//     margin-left: ${(props) => (props.Brands ? "45px" : "5px")}; */
+
 //   }
 //   @media (min-width: 768px) {
 //     font-size: 1.1rem;
@@ -35,10 +33,7 @@ import React from "react"
 //     font-size: 1.2rem;
 //     display: ${(props) => (props.hidenLarge ? "none" : "flex")};
 //   }
-//   @media (min-width: 1300px) {
-//     /* margin-right: ${(props) => (props.Brands ? "55px" : "85px")};
-//     margin-left: ${(props) => (props.Brands ? "55px" : "85px")}; */
-//   }
+
 //   @media (min-width: 1450px) {
 //     margin-left: 0px;
 //   }
@@ -71,21 +66,21 @@ import React from "react"
 // `;
 
 const BreadCrumbs = props => {
-  // const { next, only2Links, only3Links, next2, error, here, acc } = props;
-  // const accessoryBackURL = (propStr) => {
-  //   if (propStr === "Pillow") return `/accessories/list?type=pillow`;
-  //   if (propStr === "Sheets") return `/accessories/list?type=sheets`;
-  //   if (propStr === "Protector") return `/accessories/list?type=protector`;
-  //   if (propStr === "Foundation") return `/accessories/list?type=foundation`;
-  //   return null;
-  // };
-  // const accessoryDisplay = (propStr) => {
-  //   if (propStr === "Pillow") return `Pillows`;
-  //   if (propStr === "Sheets") return propStr;
-  //   if (propStr === "Protector") return `Protectors`;
-  //   if (propStr === "Foundation") return "Foundation";
-  //   return null;
-  // };
+  const { next, only2Links, only3Links, next2, error, here, acc } = props;
+  const accessoryBackURL = propStr => {
+    if (propStr === "Pillow") return `/accessories/list?type=pillow`;
+    if (propStr === "Sheets") return `/accessories/list?type=sheets`;
+    if (propStr === "Protector") return `/accessories/list?type=protector`;
+    if (propStr === "Foundation") return `/accessories/list?type=foundation`;
+    return null;
+  };
+  const accessoryDisplay = propStr => {
+    if (propStr === "Pillow") return `Pillows`;
+    if (propStr === "Sheets") return propStr;
+    if (propStr === "Protector") return `Protectors`;
+    if (propStr === "Foundation") return "Foundation";
+    return null;
+  };
   // to="/accessories/list?type=pillow"
   // "/accessories/list?type=sheets"
   // /accessories/list?type=protector
@@ -97,19 +92,18 @@ const BreadCrumbs = props => {
   //   - plural display
   //   - query URL
   return (
-    <>
-      fe
-      {/* <Crumbs to="/">Home</Crumbs>
-      <Span>&gt;</Span>
+    <div BreadWrapper>
+      <Link to="/">Home</Link>
+      <span>&gt;</span>
       {next && (
         <div>
-          <Crumbs to={`/${next.toLowerCase()}`}>{next}</Crumbs>
-          {!only2Links ? <Span>&gt;</Span> : ""}
+          <Link to={`/${next.toLowerCase()}`}>{next}</Link>
+          {!only2Links ? <span>&gt;</span> : ""}
         </div>
       )}
       {next2 && (
         <div>
-          <Crumbs
+          <Link
             to={
               acc
                 ? accessoryBackURL(next2)
@@ -117,41 +111,41 @@ const BreadCrumbs = props => {
             }
           >
             {acc ? accessoryDisplay(next2) : ifLavish(next2)}
-          </Crumbs>
-          {!only3Links ? <Span>&gt;</Span> : ""}
+          </Link>
+          {!only3Links ? <span>&gt;</span> : ""}
         </div>
       )}
-      <Location>
+      <div>
         {error ? (
-          <Crumbs
+          <Link
             to={`/${next.toLowerCase()}/${next2.toLowerCase()}/${here.toLowerCase()}`}
           >
             {here}
-          </Crumbs>
+          </Link>
         ) : (
           ifLavish(here)
         )}
-      </Location> */}
-    </>
-  )
-}
-// BreadCrumbs.defaultProps = {
-//   next: "",
-//   next2: "",
-//   only2Links: "",
-//   only3Links: "",
-//   error: "",
-//   acc: false,
-//   accProductType: "",
-// };
-// BreadCrumbs.propTypes = {
-//   only2Links: PropTypes.string,
-//   only3Links: PropTypes.string,
-//   next: PropTypes.string,
-//   next2: PropTypes.string,
-//   error: PropTypes.string,
-//   here: PropTypes.string.isRequired,
-//   acc: PropTypes.bool,
-//   accProductType: PropTypes.string,
-// };
-export default BreadCrumbs
+      </div>
+    </div>
+  );
+};
+BreadCrumbs.defaultProps = {
+  next: "",
+  next2: "",
+  only2Links: "",
+  only3Links: "",
+  error: "",
+  acc: false,
+  accProductType: "",
+};
+BreadCrumbs.propTypes = {
+  only2Links: PropTypes.string,
+  only3Links: PropTypes.string,
+  next: PropTypes.string,
+  next2: PropTypes.string,
+  error: PropTypes.string,
+  here: PropTypes.string.isRequired,
+  acc: PropTypes.bool,
+  accProductType: PropTypes.string,
+};
+export default BreadCrumbs;
