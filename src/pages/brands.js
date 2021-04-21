8016908247a9204faa1dfc2b418fd94c20518b0e
wildpow <element1028@gmail.com>
2021-04-20 18:27:46 -0700

    refactor: added back logic to brands

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -3,11 +3,38 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import Layout from "../components/Layout";
+import LifeStyleCard from "../components/Brands/LifeStyleCard";
+import BreadCrumbs from "../components/BreadCrumbs";
+import Header from "../components/ProductListing/Header";
 
 const Brands = ({ data }) => (
   <Layout>
     <HelmetDatoCms seo={data.datoCmsSeo.seoMetaTags} />
-    Brands
+    <BreadCrumbs here="Brands" hidenLarge />
+    <section>
+      <Header
+        description={data.datoCmsHeader.tagLine}
+        headerBG={data.datoCmsHeader.bgImg.url}
+        button={{ label: "Shop all Mattresses", url: "/brands/list" }}
+        title="Our Brands"
+      />
+      <div>
+        {data.allDatoCmsBrand.nodes.map(brand => (
+          <LifeStyleCard
+            key={brand.id}
+            mobileHeight="228px"
+            height="228px"
+            title={brand.displayName}
+            bgImg={brand.lifeStyleImg}
+            logo={brand.brandLogo}
+            url={`/brands/${brand.urlName}`}
+          >
+            {brand.lifeStyleText}
+          </LifeStyleCard>
+        ))}
+      </div>
+    </section>
+    <BreadCrumbs here="Brands" hidenLarge Bottom />
   </Layout>
 );
 
@@ -16,6 +43,33 @@ export const brandsSEO = graphql`
     datoCmsSeo(name: { eq: "brands" }) {
       seoMetaTags {
         ...GatsbyDatoCmsSeoMetaTags
+      }
+    }
+    datoCmsHeader(title: { eq: "AllMattressSort" }) {
+      title
+      tagLine
+      bgImg {
+        title
+        alt
+        url
+      }
+    }
+    allDatoCmsBrand(sort: { fields: position }) {
+      nodes {
+        id
+        displayName
+        urlName
+        lifeStyleText
+        lifeStyleImg {
+          alt
+          title
+          gatsbyImageData(layout: CONSTRAINED, width: 560)
+        }
+        brandLogo {
+          alt
+          title
+          url
+        }
       }
     }
   }
