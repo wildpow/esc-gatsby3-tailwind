02cc020e8381d704bb834700f904ed23b3c814e9
wildpow <element1028@gmail.com>
2021-04-22 17:25:51 -0700

    refactor: re-add data

--- src/components/Blog/BlogThumbnail.jsx
+++ src/components/Blog/BlogThumbnail.jsx
@@ -1,40 +1,37 @@
-import React from "react"
-// import PropTypes from "prop-types";
-// import { StyledLink, ThumbImg, Description } from "./PostThumbnail.styled";
+import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
 
-const PostThumbnail = ({ post, isHovering, onMouseEnter, onMouseLeave }) => {
-  return (
-    // <StyledLink
-    //   to={`/blog/${post.slug}`}
-    //   onMouseEnter={onMouseEnter}
-    //   onMouseLeave={onMouseLeave}
-    //   onTouchStart={onMouseEnter}
-    //   onTouchEnd={onMouseLeave}
-    // >
-    //   <ThumbImg
-    //     src={post.excerptImage.url}
-    //     alt={
-    //       post.excerptImage.alt === null
-    //         ? `The blog post called ${post.title}`
-    //         : post.excerptImage.alt
-    //     }
-    //     isHovering={isHovering}
-    //   />
-    //   <h3>{post.title}</h3>
-    //   <span />
-    //   <Description isHovering={isHovering}>{post.excerpt}</Description>
-    // </StyledLink>
-    <div>efwef</div>
-  )
-}
+const PostThumbnail = ({ post, isHovering, onMouseEnter, onMouseLeave }) => (
+  <Link
+    to={`/blog/${post.slug}`}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+    onTouchStart={onMouseEnter}
+    onTouchEnd={onMouseLeave}
+  >
+    <img
+      src={post.excerptImage.url}
+      alt={
+        post.excerptImage.alt === null
+          ? `The blog post called ${post.title}`
+          : post.excerptImage.alt
+      }
+      isHovering={isHovering}
+    />
+    <h3>{post.title}</h3>
+    <span />
+    <p isHovering={isHovering}>{post.excerpt}</p>
+  </Link>
+);
 
-// PostThumbnail.defaultProps = {
-//   isHovering: false,
-// };
-// PostThumbnail.propTypes = {
-//   onMouseEnter: PropTypes.func.isRequired,
-//   onMouseLeave: PropTypes.func.isRequired,
-//   isHovering: PropTypes.bool,
-//   post: PropTypes.instanceOf(Object).isRequired,
-// };
-export default PostThumbnail
+PostThumbnail.defaultProps = {
+  isHovering: false,
+};
+PostThumbnail.propTypes = {
+  onMouseEnter: PropTypes.func.isRequired,
+  onMouseLeave: PropTypes.func.isRequired,
+  isHovering: PropTypes.bool,
+  post: PropTypes.instanceOf(Object).isRequired,
+};
+export default PostThumbnail;
