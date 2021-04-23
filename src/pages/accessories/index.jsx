1a9fe66a63294884ef97dc54062f6cde4021a946
wildpow <element1028@gmail.com>
2021-04-22 17:25:24 -0700

    refactor: add back acc data

--- src/pages/accessories/index.jsx
+++ src/pages/accessories/index.jsx
@@ -1,6 +1,33 @@
-import React from "react"
-import Layout from "../../components/Layout"
+import React from "react";
+import { graphql } from "gatsby";
+import PropTypes from "prop-types";
+import Layout from "../../components/Layout";
+import Landing from "../../components/Landing";
+import BreadCrumbs from "../../components/BreadCrumbs";
 
-const AccessoryLanding = ({ data }) => <Layout>acc</Layout>
+const AccessoryLanding = ({ data }) => (
+  <Layout>
+    <BreadCrumbs here="Accessories" hidenLarge />
+    <Landing
+      acc
+      data={data.datoCmsLanding}
+      buttonName="Accessories"
+      buttonURL="/accessories/list"
+    />
+    <BreadCrumbs here="Accessories" hidenLarge Bottom Brands />
+  </Layout>
+);
 
-export default AccessoryLanding
+AccessoryLanding.propTypes = {
+  data: PropTypes.instanceOf(Object).isRequired,
+};
+
+export const AccessoryLandingPage = graphql`
+  query AccessoryLandingPage {
+    datoCmsLanding(title: { eq: "Accessory Landing" }) {
+      ...landing
+    }
+  }
+`;
+
+export default AccessoryLanding;
