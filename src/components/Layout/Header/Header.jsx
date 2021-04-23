e09984ab564b3d4b1bc01db8718bcb11e311ee30
wildpow <element1028@gmail.com>
2021-04-23 16:14:47 -0700

    chore: dep. clean up

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
 import { string, bool, func } from "prop-types";
 import Nav from "./Nav";
 import NavIcons from "./NavIcons";
