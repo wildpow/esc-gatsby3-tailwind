54adae7ebad02c719bc4af01545d402a8e7d01ee
wildpow <element1028@gmail.com>
2021-04-28 18:53:18 -0700

    style: hero home

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -1,38 +1,97 @@
 import React from "react";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
+
 import "./homeHero.css";
 
 export default function HomeHero({ heroImg, heroUrl, img1, img2, img3 }) {
   return (
-    <div className="home__hero">
-      <div className="hero__hero__main">
-        <GatsbyImage
-          image={heroImg.gatsbyImageData}
-          alt="ble"
-          // transformOptions={{ fit: "cover" }}
-        />
+    <div className="grid p-0 home__hero lg:py-5">
+      <div
+        style={{ gridArea: "hero" }}
+        className="relative flex w-full h-auto shadow-lg"
+      >
+        <GatsbyImage image={heroImg.gatsbyImageData} alt="ble" />
       </div>
-      <div className="home__hero__shop" style={{ gridArea: "shop1" }}>
-        <GatsbyImage
-          image={img1.childImageSharp.gatsbyImageData}
+
+      <Link
+        className="relative grid w-full h-auto shadow-lg home__hero__shop"
+        style={{ gridArea: "shop1" }}
+        to="/brands/list"
+      >
+        <div
+          style={{ gridArea: "1/1" }}
+          className="relative z-10 home__hero__titleCard--test-2"
+        >
+          <div className="p-4 text-2xl bg-red-800 text-blueGray-50 ">
+            Shop Mattresses
+          </div>
+        </div>
+
+        <StaticImage
+          style={{ gridArea: "1/1" }}
+          src="../../images/img2.jpeg"
           alt="ble"
-          // transformOptions={{ fit: "contain" }}
+          layout="constrained"
+          formats={["AUTO", "JPG", "WEBP", "AVIF"]}
+          width={1000}
+          height={500}
+          loading="eager"
         />
-      </div>
-      <div className="home__hero__shop" style={{ gridArea: "shop2" }}>
-        <GatsbyImage
-          image={img2.childImageSharp.gatsbyImageData}
+      </Link>
+
+      <Link
+        className="relative grid w-full h-auto home__hero__shop"
+        style={{ gridArea: "shop2" }}
+        to="/adjustable"
+      >
+        <div
+          style={{ gridArea: "1/1" }}
+          className="relative z-10 home__hero__titleCard--test"
+        >
+          {/* border-t-2 border-r-2 border-blueGray-50 */}
+          <div className="p-4 text-2xl bg-red-800 text-blueGray-50 bg-opacity-80">
+            Shop Adjustables
+          </div>
+        </div>
+
+        <StaticImage
+          style={{ gridArea: "1/1" }}
+          src="../../images/img3.jpeg"
           alt="ble"
-          // transformOptions={{ fit: "contain" }}
+          layout="constrained"
+          formats={["AUTO", "JPG", "WEBP", "AVIF"]}
+          width={1000}
+          height={500}
+          loading="eager"
         />
-      </div>
-      <div className="home__hero__shop" style={{ gridArea: "shop3" }}>
-        <GatsbyImage
-          image={img3.childImageSharp.gatsbyImageData}
+      </Link>
+
+      <Link
+        className="relative grid w-full h-auto home__hero__shop"
+        style={{ gridArea: "shop3" }}
+        to="/accessories/list"
+      >
+        <div
+          style={{ gridArea: "1/1" }}
+          className="relative z-10 home__hero__titleCard"
+        >
+          <div className="p-4 text-2xl bg-red-800 border-t-2 border-r-2 text-blueGray-50 border-blueGray-50">
+            Shop Accessories
+          </div>
+        </div>
+
+        <StaticImage
+          style={{ gridArea: "1/1" }}
+          src="../../images/img4.jpeg"
           alt="ble"
-          // transformOptions={{ fit: "contain" }}
+          layout="constrained"
+          formats={["AUTO", "JPG", "WEBP", "AVIF"]}
+          width={1000}
+          height={500}
+          loading="eager"
         />
-      </div>
+      </Link>
     </div>
   );
 }
